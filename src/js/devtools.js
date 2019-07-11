296eeb148496fd356acb00aa3c3a51ee7f5e1728
Rajasekar Venugopal <rajasekar.v@ideas2it.com>
2019-07-11 22:57:22 +0530

    Network recording

---
+++ src/js/devtools.js
@@ -0,0 +1,17 @@
+const port = chrome.runtime.connect({ name: 'devtools' });
+
+const getNetworkLog = (message) => {
+  chrome.devtools.network.getHAR(harLog => {
+    port.postMessage({ action: 'setNetworkLog', source: 'devtools', message: harLog });
+  });
+}
+
+port.onMessage.addListener(message => {
+  switch(message.action) {
+    case 'getNetworkHar':
+      getNetworkLog(message);
+      break;
+    default:
+      console.log('Unhandled message');
+  }
+});
