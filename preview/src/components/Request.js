cc5ed80086277ba8401a3300cb7769e1f08923f7
Rajasekar venugopal <rajasekar.v@ideas2it.com>
2019-07-18 06:42:21 -0700

    Request and response formatted as JSON

--- preview/src/components/Request.js
+++ preview/src/components/Request.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
+import JsonRenderer from './JsonRenderer';
 
-function Request() {
-    return (
-        <div>REQUEST</div>
-    );
+function Request(props) {
+  const { log } = JSON.parse(JSON.stringify(props));
+  const request = log.request;
+  delete log["request"];
+  delete log["response"];
+  return (
+    <div className="p-2">
+      <JsonRenderer json={log}/>
+      <h5 className="font-weight-bold my-3 border-bottom">Request</h5>
+      <JsonRenderer json={request}/>
+    </div>
+  );
 }
 
 export default Request;
