f988323da1704f278642a06a9d802580a24f2121
kalpana <kalpana.s@ideas2it.com>
2019-07-18 13:45:50 +0530

    - Network timeline

--- preview/src/App.js
+++ preview/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Row, Col, Container} from 'react-bootstrap';
 import Video from './components/Video';
 import Network from './components/Network';
-import Request from './components/Request';
+// import Request from './components/Request';
 import Response from './components/Response';
 import Console from './components/Console/Console';
 
@@ -14,29 +14,25 @@ function App() {
   return (
     <div>
       <Container fluid>
-        <Row>
+        <Row style={{"padding-top":"10px"}}>
           <Col xs lg="6">
             <Video />
           </Col>
           <Col xs lg="6">
             <Console />
           </Col>
-          <Col xs lg="6">
+        </Row>
+        </Container>
+        <Container fluid>
+        <Row>
+          <Col xs lg="6" className="network">
             <Network />
           </Col>
           <Col xs lg="6">
-            <Row>
-              <Col xs lg="6">
-                <Request />
-              </Col>
-              <Col xs lg="6">
-                <Response />
-              </Col>
-            </Row>
+            <Response />
           </Col>
         </Row>
       </Container>
-      
     </div>
   );
 }
