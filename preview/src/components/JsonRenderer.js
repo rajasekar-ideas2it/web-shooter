8ac33de79d0868d256c977d21fc2b9b412252070
Rajasekar venugopal <rajasekar.v@ideas2it.com>
2019-07-18 06:42:21 -0700

    Request and response formatted as JSON

---
+++ preview/src/components/JsonRenderer.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactJson from "react-json-view";
+
+function JsonRenderer(props) {
+  const { json } = props;
+  return (
+    <div>
+      {Object.keys(json).map(key => (
+        <div className="my-1 d-flex">
+          <span className="font-weight-bold mr-2">{`${key}: `}</span>
+          {typeof json[key] === "object" ? (
+            <ReactJson src={json[key]} displayDataTypes={false} />
+          ) : (
+            json[key]
+          )}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default JsonRenderer;
--- preview/src/components/Response.js
+++ preview/src/components/Response.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from "react";
+import JsonRenderer from "./JsonRenderer";
 
-function Response() {
-    return (
-        <div>Response</div>
-    );
+function Response(props) {
+  const {
+    log: { response }
+  } = props;
+  debugger
+  return (
+    <div className="p-2">
+      <JsonRenderer json={response} />
+    </div>
+  );
 }
 
 export default Response;
